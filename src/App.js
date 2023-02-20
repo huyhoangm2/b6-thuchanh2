893148fd5a7bec1faef05d016899dacc6fd9c8c5
huyhoangm2 <hoangken760@gmail.com>
2023-02-20 15:11:05 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  createBrowserRouter, RouterProvider,
+} from "react-router-dom";
+import ProtectedGroup from './ProtectedGroup';
+import UserProfile from './UserProfile';
+import PrivateGroup from './PrivateGroup';
+
+
+const router = createBrowserRouter([
+ {
+    path: '/private-group',
+    element: <ProtectedGroup isMember={false}><PrivateGroup /></ProtectedGroup>
+},{
+  path: '/user/:userId',
+  element: <UserProfile />
+},{
+  userId: 1293123
 }
-
-export default App;
+])
+function App () {
+return (
+  <RouterProvider router={router}/>
+)
+}
+ export default App ;
